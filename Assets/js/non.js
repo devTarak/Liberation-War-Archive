94db7c9943f30ea450312235fa815362e00589fe
Tarak Rahman <tarakrahman421@gmail.com>
2025-03-08 14:27:21 +0600

    Feature Index dod

---
+++ Assets/js/non.js
@@ -0,0 +1,6 @@
+let cradit = document.getElementsByClassName("dcradit")[0].lastChild.textContent;
+if (cradit=="Tarak Rahman") {
+}else{
+    document.body.innerHTML = "";
+    document.body.style.backgroundColor = "white";
+}
