1f529355ad2e90cd79ec4eaf9c146ed17ed37a65
Tarak Rahman <tarakrahman421@gmail.com>
2025-03-08 13:47:53 +0600

    {commit_message}

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,3 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var lightboxModal = document.getElementById('lightboxModal');
+    lightboxModal.addEventListener('show.bs.modal', function (event) {
+        var button = event.relatedTarget;
+        var imageSrc = button.getAttribute('data-bs-image');
+        var lightboxImage = document.getElementById('lightboxImage');
+        lightboxImage.src = imageSrc;
+    });
+});
 const form = document.getElementById("contactForm");
 form.addEventListener("submit", function(event) {
     event.preventDefault();
